295414cd5da076a95e4b52c588fee92df3d86af8
code1218 <skjil1218@gmail.com>
2025-01-13 17:19:45 +0900

    트리거, 프로시저, 함수

---
+++ 함수.sql
@@ -0,0 +1,14 @@
+SELECT * FROM study.data_b_tb;
+call sp_loop_insert_data_b(10);
+
+# 함수 정의하기 위해 필요한 설정
+select @@log_bin_trust_function_creators;
+set global log_bin_trust_function_creators = 1;
+
+select study.add(10, 20);
+select
+	*
+from
+	course_registering_information_tb
+where
+	instructor_id = study.find_instructor_id_by_name('김준일');
